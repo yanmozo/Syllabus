1489a0de5e30641193786c69548c4a2ded42fc7b
kiarahancajas <kiarah.ancajas@gmail.com>
2019-01-23 16:07:02 +0800

    Update DB

--- DCIS_Syllabus/Syllabus.Context.cs
+++ DCIS_Syllabus/Syllabus.Context.cs
@@ -28,11 +28,9 @@ namespace DCIS_Syllabus
         public virtual DbSet<Active_Values> Active_Values { get; set; }
         public virtual DbSet<Assessment_Criteria> Assessment_Criteria { get; set; }
         public virtual DbSet<Bibliography> Bibliographies { get; set; }
-        public virtual DbSet<Book> Books { get; set; }
         public virtual DbSet<Class_Policy> Class_Policy { get; set; }
         public virtual DbSet<Core_Value> Core_Value { get; set; }
         public virtual DbSet<Course_Deliverable> Course_Deliverable { get; set; }
-        public virtual DbSet<Course_Information> Course_Information { get; set; }
         public virtual DbSet<Course_Outcome_Addressed> Course_Outcome_Addressed { get; set; }
         public virtual DbSet<Course_Outcomes> Course_Outcomes { get; set; }
         public virtual DbSet<Grading_Rubrics> Grading_Rubrics { get; set; }
@@ -48,7 +46,6 @@ namespace DCIS_Syllabus
         public virtual DbSet<Software_Used> Software_Used { get; set; }
         public virtual DbSet<Source> Sources { get; set; }
         public virtual DbSet<Syllabu> Syllabus { get; set; }
-        public virtual DbSet<sysdiagram> sysdiagrams { get; set; }
         public virtual DbSet<User> Users { get; set; }
     }
 }
