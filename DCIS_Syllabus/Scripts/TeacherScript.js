e8b40a4f8e2d06cb9997143467b74e14a0f98f6d
Marian Mozo <yaneemozo@gmail.com>
2019-01-23 11:07:55 +0800

    - Edited in Classroom and Laboratory Policies

--- DCIS_Syllabus/Scripts/TeacherScript.js
+++ DCIS_Syllabus/Scripts/TeacherScript.js
@@ -3,3 +3,26 @@
         window.location = $(this).data("href");
     });
 });
+
+function AddNewField() {
+    var field = 
+        "<div class='input-group' id='newfield'>" +
+        "<input type='text' class='form-control' id='newPolicyInput' placeholder='Type new policy...'>" +
+        "<span class='input-group-btn'>" +
+        "<button class='btn btn-default' onclick='AddNewPolicy()' type='button'>Add</button>" +
+        "</span>" +
+        "</div><br>";
+    document.getElementById("NewInputFields").innerHTML += field;
+
+}
+
+function AddNewPolicy() {
+    var newPol = document.getElementById("newPolicyInput").value;
+    
+    var node = document.createElement("LI");
+    var textnode = document.createTextNode(newPol);
+    node.appendChild(textnode);
+    document.getElementById("PolicyList").appendChild(node);
+
+   this.style.display = "none";
+}
