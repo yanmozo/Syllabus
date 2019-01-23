fbd802ad0f8d9572b5928aa2de94d75de94f7261
Kent Michael Talisaysay <tkentmichaell@gmail.com>
2019-01-23 09:39:46 +0800

    Added InstGradAttrb Controller

---
+++ DCIS_Syllabus/Controllers/InstGradAttrbController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DCIS_Syllabus.Controllers
+{
+    public class InstGradAttrbController : Controller
+    {
+        // GET: InstGradAttrb
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult InstGradAttrb()
+        {
+            return View();
+        }
+    }
+}
