a2483293fa3548b471f420f3c482348ae9319d09
Kent Michael Talisaysay <tkentmichaell@gmail.com>
2019-01-23 10:37:28 +0800

    Added Software Used for Teacher and Coordinator

---
+++ DCIS_Syllabus/Controllers/SoftwareUsedController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DCIS_Syllabus.Controllers
+{
+    public class SoftwareUsedController : Controller
+    {
+        // GET: SoftwareUsed
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // Add/Edit Software Used
+        public ActionResult SoftwareUsed()
+        {
+            return View();
+        }
+    }
+}
